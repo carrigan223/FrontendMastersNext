fff07e80dac4638591cb31825cb1b2f345264797
Andrew Carrigan <carrigan223@gmail.com>
2020-11-28 11:09:25 -0800

    trying to get the api calls working

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,5 +1,11 @@
-export default (req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ message: "hello" }));
-};
+import nc from "next-connect";
+
+const handler = nc()
+  .get((req, res) => {
+    res.json({ message: "this is working " });
+  })
+  .post((req, res) => {
+    res.json({ message: "posted" });
+  });
+
+export default handler;
